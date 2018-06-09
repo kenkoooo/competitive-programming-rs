631c524a1d113d7b3b4907ed49b7fe62f4add975
kenkoooo <kenkou.n@gmail.com>
2018-06-09 14:29:09 +0900

    Generalized Fenwick's Tree

--- src/data_structure/fenwick_tree.rs
+++ src/data_structure/fenwick_tree.rs
@@ -1,18 +1,24 @@
+use std::ops::{AddAssign, Sub};
 /// `FenwickTree` is a data structure that can efficiently update elements
 /// and calculate prefix sums in a table of numbers.
 /// [https://en.wikipedia.org/wiki/Fenwick_tree](https://en.wikipedia.org/wiki/Fenwick_tree)
-pub struct FenwickTree {
+pub struct FenwickTree<T> {
     n: usize,
-    data: Vec<usize>,
+    data: Vec<T>,
+    init: T,
 }
 
-impl FenwickTree {
+impl<T: Copy + AddAssign + Sub<Output = T>> FenwickTree<T> {
     /// Constructs a new `FenwickTree`. The size of `FenwickTree` should be specified by `size`.
-    pub fn new(size: usize) -> FenwickTree {
-        FenwickTree { n: size + 1, data: vec![0; size + 1] }
+    pub fn new(size: usize, init: T) -> FenwickTree<T> {
+        FenwickTree {
+            n: size + 1,
+            data: vec![init; size + 1],
+            init: init,
+        }
     }
 
-    fn add(&mut self, k: usize, value: usize) {
+    pub fn add(&mut self, k: usize, value: T) {
         let mut x = k;
         while x < self.n {
             self.data[x] += value;
@@ -21,39 +27,38 @@ impl FenwickTree {
     }
 
     /// Returns a sum of range `[l, r)`
-    pub fn sum(&self, l: usize, r: usize) -> usize {
-        return self.sum_one(r) - self.sum_one(l);
+    pub fn sum(&self, l: usize, r: usize) -> T {
+        self.sum_one(r) - self.sum_one(l)
     }
 
     /// Returns a sum of range `[0, k)`
-    pub fn sum_one(&self, k: usize) -> usize {
+    pub fn sum_one(&self, k: usize) -> T {
         if k >= self.n {
             panic!("");
         }
 
-        let mut result = 0;
+        let mut result = self.init;
         let mut x = k as i32 - 1;
         while x >= 0 {
             result += self.data[x as usize];
             x = (x & (x + 1)) - 1;
         }
 
-        return result;
+        result
     }
 }
-
 
 #[cfg(test)]
 mod test {
     extern crate rand;
 
-    use super::*;
     use self::rand::Rng;
+    use super::*;
 
     #[test]
     fn random_array() {
         let n = 1000;
-        let mut bit = FenwickTree::new(n);
+        let mut bit = FenwickTree::new(n, 0);
         let mut v = vec![0; n];
 
         for _ in 0..10000 {
@@ -70,4 +75,3 @@ mod test {
         }
     }
 }
-
