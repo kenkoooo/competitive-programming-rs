dcb9a543f940c5be019bdb58d36b869042d1455c
kenkoooo <kenkou.n@gmail.com>
2018-06-09 15:32:01 +0900

    Refactored SegTree

--- src/data_structure/segment_tree.rs
+++ src/data_structure/segment_tree.rs
@@ -1,4 +1,4 @@
-/// Segment Tree for range minimum queries
+/// Segment Tree for range queries
 pub struct SegmentTree<T, F> {
     seg: Vec<T>,
     n: usize,
@@ -6,26 +6,30 @@ pub struct SegmentTree<T, F> {
     initial_value: T,
 }
 
-impl<T: Clone, F> SegmentTree<T, F> where F: Fn(T, T) -> T {
+impl<T: Copy, F> SegmentTree<T, F>
+where
+    F: Fn(T, T) -> T,
+{
     pub fn new(size: usize, initial_value: T, f: F) -> SegmentTree<T, F> {
         let mut m = 1;
         while m <= size {
             m <<= 1;
         }
         SegmentTree {
-            seg: vec![initial_value.clone(); m * 2],
+            seg: vec![initial_value; m * 2],
             n: m,
             f: f,
-            initial_value: initial_value.clone(),
+            initial_value: initial_value,
         }
     }
 
-    pub fn update(&mut self, mut k: usize, value: T) {
+    pub fn update(&mut self, k: usize, value: T) {
+        let mut k = k;
         k += self.n - 1;
         self.seg[k] = value;
         while k > 0 {
             k = (k - 1) >> 1;
-            self.seg[k] = (self.f)(self.seg[k * 2 + 1].clone(), self.seg[k * 2 + 2].clone());
+            self.seg[k] = (self.f)(self.seg[k * 2 + 1], self.seg[k * 2 + 2]);
         }
     }
 
@@ -36,19 +40,19 @@ impl<T: Clone, F> SegmentTree<T, F> where F: Fn(T, T) -> T {
     /// Panics if `a >= b`.
     pub fn query(&self, a: usize, b: usize) -> T {
         assert!(a < b);
-        return self.query_range(a, b, 0, 0, self.n);
+        self.query_range(a, b, 0, 0, self.n)
     }
 
-    pub fn query_range(&self, a: usize, b: usize, k: usize, l: usize, r: usize) -> T {
+    fn query_range(&self, a: usize, b: usize, k: usize, l: usize, r: usize) -> T {
         if r <= a || b <= l {
-            return self.initial_value.clone();
+            self.initial_value
+        } else if a <= l && r <= b {
+            self.seg[k]
+        } else {
+            let x = self.query_range(a, b, k * 2 + 1, l, (l + r) >> 1);
+            let y = self.query_range(a, b, k * 2 + 2, (l + r) >> 1, r);
+            (self.f)(x, y)
         }
-        if a <= l && r <= b {
-            return self.seg[k].clone();
-        }
-        let x = self.query_range(a, b, k * 2 + 1, l, (l + r) >> 1);
-        let y = self.query_range(a, b, k * 2 + 2, (l + r) >> 1, r);
-        (self.f)(x, y)
     }
 }
 
@@ -56,18 +60,20 @@ impl<T: Clone, F> SegmentTree<T, F> where F: Fn(T, T) -> T {
 mod test {
     extern crate rand;
 
-    use super::*;
     use self::rand::Rng;
-    use test::Bencher;
-    use std::i64::MAX;
+    use super::*;
     use std::cmp;
+    use std::i64::MAX;
+    use test::Bencher;
 
     #[test]
     fn random_array() {
         let n = 1000;
-        let arr = (0..n).map(|_| {
-            return rand::thread_rng().gen::<i64>();
-        }).collect::<Vec<_>>();
+        let arr = (0..n)
+            .map(|_| {
+                return rand::thread_rng().gen::<i64>();
+            })
+            .collect::<Vec<_>>();
 
         let mut seg = SegmentTree::new(n, MAX, |a, b| cmp::min(a, b));
         for i in 0..n {
