c46e5143816da4737f1b90a3c31d12e3839da875
Kenko Nakamura <kenkou.n@gmail.com>
2018-11-11 17:53:50 +0900

    Supported SubAssign in ModInt, resolved #6

--- src/math/mod_int.rs
+++ src/math/mod_int.rs
@@ -1,187 +1,175 @@
-use num_traits;
+pub mod mod_int {
+    use std::ops::{Add, AddAssign, Mul, MulAssign, Rem, Sub, SubAssign};
 
-use std::ops::{Add, AddAssign, BitAnd, DivAssign, Mul, MulAssign, Rem, ShrAssign, Sub};
-
-#[derive(Copy)]
-pub struct ModInt<T> {
-    value: T,
-    modulo: T,
-}
-
-impl<T> Clone for ModInt<T>
-where
-    T: Copy,
-{
-    fn clone(&self) -> Self {
-        ModInt {
-            value: self.value,
-            modulo: self.modulo,
-        }
+    #[derive(Copy)]
+    pub struct ModInt<T> {
+        pub value: T,
+        modulo: T,
     }
 
-    fn clone_from(&mut self, source: &ModInt<T>) {
-        self.value = source.value;
-        self.modulo = source.modulo;
-    }
-}
-
-impl<T> Add<ModInt<T>> for ModInt<T>
-where
-    T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
-{
-    type Output = ModInt<T>;
-    fn add(self, rhs: ModInt<T>) -> ModInt<T> {
-        self + rhs.value
-    }
-}
-
-impl<T> Add<T> for ModInt<T>
-where
-    T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
-{
-    type Output = ModInt<T>;
-    fn add(self, rhs: T) -> ModInt<T> {
-        let m = self.modulo;
-        let mut t = rhs + self.value;
-        if t > m {
-            t = t - m;
-        }
-        ModInt {
-            value: t,
-            modulo: self.modulo,
-        }
-    }
-}
-
-impl<T> Sub<T> for ModInt<T>
-where
-    T: PartialOrd + Copy + Add<Output = T> + Sub<Output = T> + Rem<Output = T>,
-{
-    type Output = ModInt<T>;
-    fn sub(self, rhs: T) -> ModInt<T> {
-        let rhs = if rhs >= self.modulo {
-            rhs % self.modulo
-        } else {
-            rhs
-        };
-        let value = if self.value < rhs {
-            self.value + self.modulo
-        } else {
-            self.value
-        };
-        ModInt {
-            value: value - rhs,
-            modulo: self.modulo,
-        }
-    }
-}
-
-impl<T> Sub<ModInt<T>> for ModInt<T>
-where
-    T: PartialOrd + Copy + Add<Output = T> + Sub<Output = T> + Rem<Output = T>,
-{
-    type Output = ModInt<T>;
-    fn sub(self, rhs: ModInt<T>) -> ModInt<T> {
-        self - rhs.value
-    }
-}
-
-impl<T> AddAssign<T> for ModInt<T>
-where
-    T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
-{
-    fn add_assign(&mut self, other: T) {
-        *self = *self + other;
-    }
-}
-impl<T> AddAssign<ModInt<T>> for ModInt<T>
-where
-    T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
-{
-    fn add_assign(&mut self, other: ModInt<T>) {
-        *self = *self + other;
-    }
-}
-
-impl<T> Mul<ModInt<T>> for ModInt<T>
-where
-    T: Mul<Output = T> + Rem<Output = T> + Copy,
-{
-    type Output = ModInt<T>;
-
-    fn mul(self, rhs: ModInt<T>) -> ModInt<T> {
-        self * rhs.value
-    }
-}
-impl<T> Mul<T> for ModInt<T>
-where
-    T: Mul<Output = T> + Rem<Output = T> + Copy,
-{
-    type Output = ModInt<T>;
-
-    fn mul(self, rhs: T) -> ModInt<T> {
-        let t = (self.value * rhs) % self.modulo;
-        ModInt {
-            value: t,
-            modulo: self.modulo,
-        }
-    }
-}
-
-impl<T> MulAssign<T> for ModInt<T>
-where
-    T: Mul<Output = T> + Rem<Output = T> + Copy,
-{
-    fn mul_assign(&mut self, rhs: T) {
-        *self = *self * rhs;
-    }
-}
-
-impl<T> MulAssign<ModInt<T>> for ModInt<T>
-where
-    T: Mul<Output = T> + Rem<Output = T> + Copy,
-{
-    fn mul_assign(&mut self, rhs: ModInt<T>) {
-        *self = *self * rhs;
-    }
-}
-
-impl<T> ModInt<T> {
-    pub fn new(value: T, modulo: T) -> ModInt<T> {
-        ModInt {
-            value: value,
-            modulo: modulo,
-        }
-    }
-}
-
-impl<T> ModInt<T>
-where
-    T: num_traits::Zero
-        + num_traits::One
-        + Copy
-        + PartialOrd
-        + BitAnd<Output = T>
-        + DivAssign
-        + ShrAssign
-        + Mul<Output = T>
-        + Rem<Output = T>,
-{
-    pub fn pow(&self, e: T) -> ModInt<T> {
-        let mut result = ModInt {
-            value: num_traits::one(),
-            modulo: self.modulo,
-        };
-        let mut cur = *self;
-        let mut e = e;
-        while e > num_traits::zero() {
-            if e & num_traits::one() != num_traits::zero() {
-                result *= cur;
+    impl<T> Clone for ModInt<T>
+    where
+        T: Copy,
+    {
+        fn clone(&self) -> Self {
+            ModInt {
+                value: self.value,
+                modulo: self.modulo,
             }
-            e >>= num_traits::one();
-            cur *= cur;
         }
-        result
+
+        fn clone_from(&mut self, source: &ModInt<T>) {
+            self.value = source.value;
+            self.modulo = source.modulo;
+        }
+    }
+
+    impl<T> Add<ModInt<T>> for ModInt<T>
+    where
+        T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
+    {
+        type Output = ModInt<T>;
+        fn add(self, rhs: ModInt<T>) -> ModInt<T> {
+            self + rhs.value
+        }
+    }
+
+    impl<T> Add<T> for ModInt<T>
+    where
+        T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
+    {
+        type Output = ModInt<T>;
+        fn add(self, rhs: T) -> ModInt<T> {
+            let m = self.modulo;
+            let mut t = rhs + self.value;
+            if t > m {
+                t = t - m;
+            }
+            ModInt {
+                value: t,
+                modulo: self.modulo,
+            }
+        }
+    }
+
+    impl<T> Sub<T> for ModInt<T>
+    where
+        T: PartialOrd + Copy + Add<Output = T> + Sub<Output = T> + Rem<Output = T>,
+    {
+        type Output = ModInt<T>;
+        fn sub(self, rhs: T) -> ModInt<T> {
+            let rhs = if rhs >= self.modulo {
+                rhs % self.modulo
+            } else {
+                rhs
+            };
+            let value = if self.value < rhs {
+                self.value + self.modulo
+            } else {
+                self.value
+            };
+            ModInt {
+                value: value - rhs,
+                modulo: self.modulo,
+            }
+        }
+    }
+
+    impl<T> Sub<ModInt<T>> for ModInt<T>
+    where
+        T: PartialOrd + Copy + Add<Output = T> + Sub<Output = T> + Rem<Output = T>,
+    {
+        type Output = ModInt<T>;
+        fn sub(self, rhs: ModInt<T>) -> ModInt<T> {
+            self - rhs.value
+        }
+    }
+
+    impl<T> AddAssign<T> for ModInt<T>
+    where
+        T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
+    {
+        fn add_assign(&mut self, other: T) {
+            *self = *self + other;
+        }
+    }
+    impl<T> AddAssign<ModInt<T>> for ModInt<T>
+    where
+        T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
+    {
+        fn add_assign(&mut self, other: ModInt<T>) {
+            *self = *self + other;
+        }
+    }
+
+    impl<T> SubAssign<T> for ModInt<T>
+    where
+        T: PartialOrd + Copy + Add<Output = T> + Sub<Output = T> + Rem<Output = T>,
+    {
+        fn sub_assign(&mut self, other: T) {
+            *self = *self - other;
+        }
+    }
+
+    impl<T> SubAssign<ModInt<T>> for ModInt<T>
+    where
+        T: PartialOrd + Copy + Add<Output = T> + Sub<Output = T> + Rem<Output = T>,
+    {
+        fn sub_assign(&mut self, other: ModInt<T>) {
+            *self = *self - other;
+        }
+    }
+
+    impl<T> Mul<ModInt<T>> for ModInt<T>
+    where
+        T: Mul<Output = T> + Rem<Output = T> + Copy,
+    {
+        type Output = ModInt<T>;
+
+        fn mul(self, rhs: ModInt<T>) -> ModInt<T> {
+            self * rhs.value
+        }
+    }
+    impl<T> Mul<T> for ModInt<T>
+    where
+        T: Mul<Output = T> + Rem<Output = T> + Copy,
+    {
+        type Output = ModInt<T>;
+
+        fn mul(self, rhs: T) -> ModInt<T> {
+            let t = (self.value * rhs) % self.modulo;
+            ModInt {
+                value: t,
+                modulo: self.modulo,
+            }
+        }
+    }
+
+    impl<T> MulAssign<T> for ModInt<T>
+    where
+        T: Mul<Output = T> + Rem<Output = T> + Copy,
+    {
+        fn mul_assign(&mut self, rhs: T) {
+            *self = *self * rhs;
+        }
+    }
+
+    impl<T> MulAssign<ModInt<T>> for ModInt<T>
+    where
+        T: Mul<Output = T> + Rem<Output = T> + Copy,
+    {
+        fn mul_assign(&mut self, rhs: ModInt<T>) {
+            *self = *self * rhs;
+        }
+    }
+
+    impl<T> ModInt<T> {
+        pub fn new(value: T, modulo: T) -> ModInt<T> {
+            ModInt {
+                value: value,
+                modulo: modulo,
+            }
+        }
     }
 }
 
@@ -190,8 +178,7 @@ mod test {
     extern crate rand;
 
     use self::rand::distributions::{IndependentSample, Range};
-    use self::rand::Rng;
-    use super::*;
+    use super::mod_int::*;
 
     #[test]
     fn random_add_sub() {
@@ -219,23 +206,14 @@ mod test {
             mx += y;
             x += y;
             assert_eq!(mx.value, x % modulo);
-        }
-    }
-    #[test]
-    fn random_pow() {
-        let modulo = 1_000_000_007;
-        for _ in 0..1000 {
-            let x: usize = rand::thread_rng().gen_range(1, 1000);
-            let e: usize = rand::thread_rng().gen_range(1, 1000);
 
-            let mut result = 1;
-            for _ in 0..e {
-                result *= x;
-                result %= modulo;
-            }
+            mx -= my;
+            x = (x + modulo - y % modulo) % modulo;
+            assert_eq!(mx.value, x);
 
-            let mx = ModInt::new(x, modulo);
-            assert_eq!(result, mx.pow(e).value);
+            mx -= y;
+            x = (x + modulo - y % modulo) % modulo;
+            assert_eq!(mx.value, x);
         }
     }
 
