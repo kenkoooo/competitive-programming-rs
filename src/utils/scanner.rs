83a6c59d4ab0b7f3204927d996a4c67e39c63192
kenkoooo <kenkou.n@gmail.com>
2018-11-14 14:47:22 +0900

    Refactoring scanner

--- src/utils/scanner.rs
+++ src/utils/scanner.rs
@@ -1,10 +1,12 @@
-pub struct Scanner<R>(R);
+pub struct Scanner<R> {
+    reader: R,
+}
 
 impl<R: std::io::Read> Scanner<R> {
     pub fn read<T: std::str::FromStr>(&mut self) -> T {
         use std::io::Read;
         let buf = self
-            .0
+            .reader
             .by_ref()
             .bytes()
             .map(|b| b.unwrap())
@@ -32,7 +34,7 @@ mod tests {
     #[test]
     fn scanner_test() {
         let cursor = io::Cursor::new(b"1 a 0.1");
-        let mut sc = Scanner(cursor);
+        let mut sc = Scanner { reader: cursor };
 
         assert_eq!(1, sc.read());
         assert_eq!("a".to_string(), sc.read::<String>());
