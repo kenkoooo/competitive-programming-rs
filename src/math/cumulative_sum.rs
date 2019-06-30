129929564ae3bb1e333c8d47de0bc766f13a7861
kenkoooo <kenkou.n@gmail.com>
2019-06-30 19:30:57 +0900

    Using <T> in CumlativeSum

--- src/math/cumulative_sum.rs
+++ src/math/cumulative_sum.rs
@@ -1,16 +1,18 @@
-use std::cmp;
-
-pub struct CumulativeSum {
+pub struct CumulativeSum<T> {
     ny: usize,
     nx: usize,
-    sum: Vec<Vec<usize>>,
+    sum: Vec<Vec<T>>,
 }
 
-impl CumulativeSum {
-    pub fn new(a: &Vec<Vec<usize>>) -> CumulativeSum {
+impl<T> CumulativeSum<T>
+where
+    T: Copy + std::ops::Add<Output = T> + std::ops::Sub<Output = T>,
+{
+    pub fn new(a: &Vec<Vec<T>>, init: T) -> CumulativeSum<T> {
+        assert!(a.len() > 0);
         let ny = a.len();
         let nx = a[0].len();
-        let mut sum = vec![vec![0; nx + 1]; ny + 1];
+        let mut sum = vec![vec![init; nx + 1]; ny + 1];
         for i in 0..ny {
             for j in 0..nx {
                 sum[i + 1][j + 1] = a[i][j] + sum[i][j + 1] + sum[i + 1][j] - sum[i][j];
@@ -23,12 +25,10 @@ impl CumulativeSum {
         }
     }
 
-    pub fn get_sum(&self, y1: usize, x1: usize, y2: usize, x2: usize) -> usize {
-        if y1 > y2 || x1 > x2 {
-            return 0;
-        }
-        let y2 = cmp::min(y2, self.ny - 1);
-        let x2 = cmp::min(x2, self.nx - 1);
+    pub fn get_sum(&self, y1: usize, x1: usize, y2: usize, x2: usize) -> T {
+        assert!(y1 <= y2 && x1 <= x2);
+        assert!(y2 <= self.ny - 1);
+        assert!(x2 <= self.nx - 1);
         return self.sum[y2 + 1][x2 + 1] + self.sum[y1][x1]
             - self.sum[y1][x2 + 1]
             - self.sum[y2 + 1][x1];
@@ -56,7 +56,7 @@ mod test {
                 }
             }
 
-            let sum = CumulativeSum::new(&array);
+            let sum = CumulativeSum::new(&array, 0);
             for i in 0..h {
                 for j in 0..w {
                     for i2 in i..h {
