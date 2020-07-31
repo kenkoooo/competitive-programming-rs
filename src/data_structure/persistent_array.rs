0fb55e09496ce3eba2aa5049c33087d1f8dd4825
kenkoooo <kenkou.n@gmail.com>
2020-08-01 07:41:07 +0900

    Rc persistent_array

--- src/data_structure/persistent_array.rs
+++ src/data_structure/persistent_array.rs
@@ -1,56 +1,53 @@
 pub mod persistent_array {
+    use std::rc::Rc;
+
     const N: usize = 20;
-    pub struct PersistentArray<T: Clone> {
-        nodes: Vec<Node<T>>,
-    }
     #[derive(Clone)]
-    struct Node<T: Clone> {
+    pub struct Node<T: Clone> {
         data: Option<T>,
-        children: [Option<usize>; N],
+        children: [Option<Rc<Node<T>>>; N],
     }
+
     impl<T: Clone> Node<T> {
-        fn new(data: Option<T>, children: [Option<usize>; N]) -> Self {
+        pub fn new(data: Option<T>, children: [Option<Rc<Node<T>>>; N]) -> Self {
             Self { data, children }
         }
     }
 
-    impl<T: Clone> PersistentArray<T> {
-        pub fn new() -> Self {
+    impl<T: Clone> Default for Node<T> {
+        fn default() -> Self {
             Self {
-                nodes: vec![Node::new(None, [None; N])],
+                data: None,
+                children: Default::default(),
             }
         }
-        pub fn set(&mut self, index: usize, data: T, node_id: usize) -> usize {
-            if index == 0 {
-                let new_node_id = self.nodes.len();
-                self.nodes
-                    .push(Node::new(Some(data), self.nodes[node_id].children.clone()));
-                new_node_id
-            } else {
-                let a = match self.nodes[node_id].children[index % N] {
-                    Some(next_node_id) => self.set(index / N, data, next_node_id),
-                    None => {
-                        let intermediate_node_id = self.nodes.len();
-                        self.nodes.push(Node::new(None, [None; N]));
-                        self.nodes[node_id].children[index % N] = Some(intermediate_node_id);
-                        self.set(index / N, data, intermediate_node_id)
-                    }
-                };
-                let new_node_id = self.nodes.len();
-                self.nodes.push(self.nodes[node_id].clone());
-                self.nodes[new_node_id].children[index % N] = Some(a);
-                new_node_id
-            }
-        }
+    }
 
-        pub fn get(&self, index: usize, node_id: usize) -> Option<&T> {
-            if index == 0 {
-                self.nodes[node_id].data.as_ref()
-            } else {
-                match self.nodes[node_id].children[index % N] {
-                    Some(next_node_id) => self.get(index / N, next_node_id),
-                    None => None,
+    pub fn set<T: Clone + Copy>(index: usize, data: T, node: Rc<Node<T>>) -> Rc<Node<T>> {
+        if index == 0 {
+            let new_node = Node::new(Some(data), node.children.clone());
+            Rc::new(new_node)
+        } else {
+            let a = match node.children[index % N].as_ref() {
+                Some(next_node) => set(index / N, data, next_node.clone()),
+                None => {
+                    let intermediate_node = Node::default();
+                    set(index / N, data, Rc::new(intermediate_node))
                 }
+            };
+            let mut new_node = node.as_ref().clone();
+            new_node.children[index % N] = Some(a);
+            Rc::new(new_node)
+        }
+    }
+
+    pub fn get<T: Clone>(index: usize, node: Rc<Node<T>>) -> Option<T> {
+        if index == 0 {
+            node.data.clone()
+        } else {
+            match node.children[index % N].as_ref() {
+                Some(next_node) => get(index / N, next_node.clone()),
+                None => None,
             }
         }
     }
@@ -58,45 +55,45 @@ pub mod persistent_array {
 
 #[cfg(test)]
 mod tests {
-    use crate::data_structure::persistent_array::persistent_array::PersistentArray;
+    use crate::data_structure::persistent_array::persistent_array::{get, set, Node};
     use rand::distributions::Uniform;
     use rand::{thread_rng, Rng};
+    use std::rc::Rc;
 
     #[test]
     fn test_persistent_array() {
         let mut rng = thread_rng();
-        let mut array: PersistentArray<i64> = PersistentArray::new();
         let n: usize = 20;
         let mut vs = vec![vec![None; n]];
-        let mut node_ids = vec![0];
+        let mut nodes = vec![Rc::new(Node::default())];
 
         for _ in 0..10000 {
             let pick = rng.sample(Uniform::from(0..vs.len()));
             let mut new_vec = vs[pick].clone();
-            let node_id = node_ids[pick];
+            let node = nodes[pick].clone();
             let pos = rng.sample(Uniform::from(0..n));
             let value: i64 = rng.gen();
             new_vec[pos] = Some(value);
-            let new_node_id = array.set(pos, value, node_id);
+            let new_node = set(pos, value, node);
 
             for i in 0..n {
                 let expected = new_vec[i];
-                let actual = array.get(i, new_node_id);
-                assert_eq!(expected, actual.cloned());
+                let actual = get(i, new_node.clone());
+                assert_eq!(expected, actual);
             }
             vs.push(new_vec.clone());
-            node_ids.push(new_node_id);
+            nodes.push(new_node.clone());
 
             let value: i64 = rng.gen();
             new_vec[pos] = Some(value);
-            let new_node_id = array.set(pos, value, new_node_id);
+            let new_node = set(pos, value, new_node);
             for i in 0..n {
                 let expected = new_vec[i];
-                let actual = array.get(i, new_node_id);
-                assert_eq!(expected, actual.cloned());
+                let actual = get(i, new_node.clone());
+                assert_eq!(expected, actual);
             }
             vs.push(new_vec.clone());
-            node_ids.push(new_node_id);
+            nodes.push(new_node);
         }
     }
 }
