d1ecdd302572c55830b0169bf041f1df1ecbcd02
kenkoooo <kenkou.n@gmail.com>
2020-08-24 00:28:30 +0900

    Improve CumulativeSum's interface

--- src/math/cumulative_sum.rs
+++ src/math/cumulative_sum.rs
@@ -1,8 +1,4 @@
-pub struct CumulativeSum<T> {
-    ny: usize,
-    nx: usize,
-    sum: Vec<Vec<T>>,
-}
+pub struct CumulativeSum<T>(Vec<Vec<T>>);
 
 impl<T> CumulativeSum<T>
 where
@@ -10,22 +6,23 @@ where
 {
     pub fn new(a: &[Vec<T>], init: T) -> CumulativeSum<T> {
         assert!(!a.is_empty());
-        let ny = a.len();
-        let nx = a[0].len();
-        let mut sum = vec![vec![init; nx + 1]; ny + 1];
-        for i in 0..ny {
-            for j in 0..nx {
+        let h = a.len();
+        let w = a[0].len();
+        let mut sum = vec![vec![init; w + 1]; h + 1];
+        for i in 0..h {
+            for j in 0..w {
                 sum[i + 1][j + 1] = a[i][j] + sum[i][j + 1] + sum[i + 1][j] - sum[i][j];
             }
         }
-        CumulativeSum { ny, nx, sum }
+        CumulativeSum(sum)
     }
 
-    pub fn get_sum(&self, y1: usize, x1: usize, y2: usize, x2: usize) -> T {
-        assert!(y1 <= y2 && x1 <= x2);
-        assert!(y2 < self.ny);
-        assert!(x2 < self.nx);
-        self.sum[y2 + 1][x2 + 1] + self.sum[y1][x1] - self.sum[y1][x2 + 1] - self.sum[y2 + 1][x1]
+    pub fn get_sum(&self, h_range: std::ops::Range<usize>, w_range: std::ops::Range<usize>) -> T {
+        assert!(h_range.end <= self.0.len());
+        assert!(w_range.end <= self.0[0].len());
+        self.0[h_range.end][w_range.end] + self.0[h_range.start][w_range.start]
+            - self.0[h_range.start][w_range.end]
+            - self.0[h_range.end][w_range.start]
     }
 }
 
@@ -51,18 +48,18 @@ mod test {
             }
 
             let sum = CumulativeSum::new(&array, 0);
-            for i in 0..h {
-                for j in 0..w {
-                    for i2 in i..h {
-                        for j2 in j..w {
+            for from_i in 0..h {
+                for to_i in (from_i + 1)..=h {
+                    for from_j in 0..w {
+                        for to_j in (from_j + 1)..=w {
                             let mut check = 0;
-                            for k in i..(i2 + 1) {
-                                for l in j..(j2 + 1) {
-                                    check += array[k][l];
+                            for i in from_i..to_i {
+                                for j in from_j..to_j {
+                                    check += array[i][j];
                                 }
                             }
 
-                            assert_eq!(check, sum.get_sum(i, j, i2, j2));
+                            assert_eq!(check, sum.get_sum(from_i..to_i, from_j..to_j));
                         }
                     }
                 }
