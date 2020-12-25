4a65fd730ebfc2bc89697563364914bb06134f8d
kenkoooo <kenkou.n@gmail.com>
2020-12-25 15:19:10 +0900

    Remove traits

--- src/data_structure/lazy_segment_tree.rs
+++ src/data_structure/lazy_segment_tree.rs
@@ -1,51 +1,52 @@
 pub mod lazy_segment_tree {
     type Range = std::ops::Range<usize>;
-    pub trait Monoid {
-        type S: Clone;
-        fn identity() -> Self::S;
-        fn binary_operation(a: &Self::S, b: &Self::S) -> Self::S;
-    }
 
-    pub trait MapMonoid {
-        type M: Monoid;
-        type F: Clone;
-        fn identity_element() -> <Self::M as Monoid>::S {
-            Self::M::identity()
-        }
-        fn binary_operation(
-            a: &<Self::M as Monoid>::S,
-            b: &<Self::M as Monoid>::S,
-        ) -> <Self::M as Monoid>::S {
-            Self::M::binary_operation(a, b)
-        }
-        fn identity_map() -> Self::F;
-        fn mapping(f: &Self::F, x: &<Self::M as Monoid>::S) -> <Self::M as Monoid>::S;
-        fn composition(f: &Self::F, g: &Self::F) -> Self::F;
-    }
-
-    pub struct LazySegmentTree<F>
-    where
-        F: MapMonoid,
-    {
+    pub struct LazySegmentTree<S, Op, E, F, Mapping, Composition, Id> {
         n: usize,
         size: usize,
         log: usize,
-        data: Vec<<F::M as Monoid>::S>,
-        lazy: Vec<F::F>,
+        data: Vec<S>,
+        lazy: Vec<F>,
+        op: Op,
+        e: E,
+        mapping: Mapping,
+        composition: Composition,
+        id: Id,
     }
 
-    impl<F: MapMonoid> LazySegmentTree<F> {
-        pub fn new(n: usize) -> Self {
+    impl<S, Op, E, F, Mapping, Composition, Id> LazySegmentTree<S, Op, E, F, Mapping, Composition, Id>
+    where
+        S: Clone,
+        E: Fn() -> S,
+        F: Clone,
+        Op: Fn(&S, &S) -> S,
+        Mapping: Fn(&F, &S) -> S,
+        Composition: Fn(&F, &F) -> F,
+        Id: Fn() -> F,
+    {
+        pub fn new(
+            n: usize,
+            e: E,
+            op: Op,
+            mapping: Mapping,
+            composition: Composition,
+            id: Id,
+        ) -> Self {
             let size = n.next_power_of_two() as usize;
             LazySegmentTree {
                 n,
                 size,
                 log: size.trailing_zeros() as usize,
-                data: vec![F::identity_element(); 2 * size],
-                lazy: vec![F::identity_map(); size],
+                data: vec![e(); 2 * size],
+                lazy: vec![id(); size],
+                e,
+                op,
+                mapping,
+                composition,
+                id,
             }
         }
-        pub fn set(&mut self, mut index: usize, value: <F::M as Monoid>::S) {
+        pub fn set(&mut self, mut index: usize, value: S) {
             assert!(index < self.n);
             index += self.size;
             for i in (1..=self.log).rev() {
@@ -57,7 +58,7 @@ pub mod lazy_segment_tree {
             }
         }
 
-        pub fn get(&mut self, mut index: usize) -> <F::M as Monoid>::S {
+        pub fn get(&mut self, mut index: usize) -> S {
             assert!(index < self.n);
             index += self.size;
             for i in (1..=self.log).rev() {
@@ -66,7 +67,7 @@ pub mod lazy_segment_tree {
             self.data[index].clone()
         }
 
-        pub fn prod(&mut self, range: Range) -> <F::M as Monoid>::S {
+        pub fn prod(&mut self, range: Range) -> S {
             let mut l = range.start;
             let mut r = range.end;
             assert!(l < r && r <= self.n);
@@ -83,40 +84,40 @@ pub mod lazy_segment_tree {
                 }
             }
 
-            let mut sum_l = F::identity_element();
-            let mut sum_r = F::identity_element();
+            let mut sum_l = (self.e)();
+            let mut sum_r = (self.e)();
             while l < r {
                 if l & 1 != 0 {
-                    sum_l = F::binary_operation(&sum_l, &self.data[l]);
+                    sum_l = (self.op)(&sum_l, &self.data[l]);
                     l += 1;
                 }
                 if r & 1 != 0 {
                     r -= 1;
-                    sum_r = F::binary_operation(&self.data[r], &sum_r);
+                    sum_r = (self.op)(&self.data[r], &sum_r);
                 }
                 l >>= 1;
                 r >>= 1;
             }
 
-            F::binary_operation(&sum_l, &sum_r)
+            (self.op)(&sum_l, &sum_r)
         }
 
-        pub fn all_prod(&self) -> <F::M as Monoid>::S {
+        pub fn all_prod(&self) -> S {
             self.data[1].clone()
         }
 
-        pub fn apply(&mut self, mut index: usize, f: F::F) {
+        pub fn apply(&mut self, mut index: usize, f: F) {
             assert!(index < self.n);
             index += self.size;
             for i in (1..=self.log).rev() {
                 self.push(index >> i);
             }
-            self.data[index] = F::mapping(&f, &self.data[index]);
+            self.data[index] = (self.mapping)(&f, &self.data[index]);
             for i in 1..=self.log {
                 self.update(index >> i);
             }
         }
-        pub fn apply_range(&mut self, range: Range, f: F::F) {
+        pub fn apply_range(&mut self, range: Range, f: F) {
             let mut l = range.start;
             let mut r = range.end;
             assert!(l <= r && r <= self.n);
@@ -162,25 +163,20 @@ pub mod lazy_segment_tree {
                 }
             }
         }
-    }
 
-    impl<F> LazySegmentTree<F>
-    where
-        F: MapMonoid,
-    {
         fn update(&mut self, k: usize) {
-            self.data[k] = F::binary_operation(&self.data[2 * k], &self.data[2 * k + 1]);
+            self.data[k] = (self.op)(&self.data[2 * k], &self.data[2 * k + 1]);
         }
-        fn all_apply(&mut self, k: usize, f: F::F) {
-            self.data[k] = F::mapping(&f, &self.data[k]);
+        fn all_apply(&mut self, k: usize, f: F) {
+            self.data[k] = (self.mapping)(&f, &self.data[k]);
             if k < self.size {
-                self.lazy[k] = F::composition(&f, &self.lazy[k]);
+                self.lazy[k] = (self.composition)(&f, &self.lazy[k]);
             }
         }
         fn push(&mut self, k: usize) {
             self.all_apply(2 * k, self.lazy[k].clone());
             self.all_apply(2 * k + 1, self.lazy[k].clone());
-            self.lazy[k] = F::identity_map();
+            self.lazy[k] = (self.id)();
         }
     }
 }
@@ -189,67 +185,21 @@ pub mod lazy_segment_tree {
 mod test {
     use super::lazy_segment_tree::*;
     use rand::Rng;
-    use std::cmp;
+    use std::cmp::{max, min};
 
     const INF: i64 = 1 << 60;
-
-    struct MinAdd;
-    impl Monoid for MinAdd {
-        type S = i64;
-        fn identity() -> Self::S {
-            INF
-        }
-        fn binary_operation(a: &Self::S, b: &Self::S) -> Self::S {
-            std::cmp::min(*a, *b)
-        }
-    }
-    impl MapMonoid for MinAdd {
-        type M = Self;
-        type F = i64;
-
-        fn identity_map() -> Self::F {
-            0
-        }
-
-        fn mapping(f: &Self::F, x: &<Self::M as Monoid>::S) -> <Self::M as Monoid>::S {
-            *f + *x
-        }
-
-        fn composition(f: &Self::F, g: &Self::F) -> Self::F {
-            *f + *g
-        }
-    }
-    struct MaxAdd;
-    impl Monoid for MaxAdd {
-        type S = i64;
-        fn identity() -> Self::S {
-            -INF
-        }
-        fn binary_operation(a: &Self::S, b: &Self::S) -> Self::S {
-            std::cmp::max(*a, *b)
-        }
-    }
-    impl MapMonoid for MaxAdd {
-        type M = Self;
-        type F = i64;
-
-        fn identity_map() -> Self::F {
-            0
-        }
-
-        fn mapping(f: &Self::F, x: &<Self::M as Monoid>::S) -> <Self::M as Monoid>::S {
-            *f + *x
-        }
-
-        fn composition(f: &Self::F, g: &Self::F) -> Self::F {
-            *f + *g
-        }
-    }
 
     #[test]
     fn edge_case() {
         let n = 5;
-        let mut seg_min: LazySegmentTree<MinAdd> = LazySegmentTree::new(n);
+        let mut seg_min = LazySegmentTree::new(
+            n,
+            || INF,
+            |s, t| min(*s, *t),
+            |f, x| *f + *x,
+            |f, g| *f + *g,
+            || 0,
+        );
         let mut values = vec![0; n];
         for i in 0..n {
             values[i] = i as i64;
@@ -283,8 +233,22 @@ mod test {
     fn random_add() {
         let n = 32;
         let mut array = vec![0; n];
-        let mut seg_min: LazySegmentTree<MinAdd> = LazySegmentTree::new(n);
-        let mut seg_max: LazySegmentTree<MaxAdd> = LazySegmentTree::new(n);
+        let mut seg_min = LazySegmentTree::new(
+            n,
+            || INF,
+            |s, t| min(*s, *t),
+            |f, x| *f + *x,
+            |f, g| *f + *g,
+            || 0,
+        );
+        let mut seg_max = LazySegmentTree::new(
+            n,
+            || -INF,
+            |s, t| max(*s, *t),
+            |f, x| *f + *x,
+            |f, g| *f + *g,
+            || 0,
+        );
         for i in 0..n {
             let value = rand::thread_rng().gen::<i16>() as i64;
             array[i] = value;
@@ -307,8 +271,8 @@ mod test {
                         let mut min = INF;
                         let mut max = -INF;
                         for i in l..r {
-                            min = cmp::min(min, array[i]);
-                            max = cmp::max(max, array[i]);
+                            min = std::cmp::min(min, array[i]);
+                            max = std::cmp::max(max, array[i]);
                         }
 
                         assert_eq!(seg_min.prod(l..r), min);
