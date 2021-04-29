1ff55437dc54174f8074308c21ad04ff59494a02
kenkoooo <kenkou.n@gmail.com>
2021-04-29 14:19:07 +0900

    Fix treap

--- src/data_structure/treap.rs
+++ src/data_structure/treap.rs
@@ -1,9 +1,82 @@
 pub mod treap {
-    use std::cmp::Ordering;
-    use std::cmp::Ordering::*;
-    use std::mem::swap;
-
     type BNode<T> = Box<Node<T>>;
+    pub struct Treap<T> {
+        rng: XorShift,
+        root: Option<BNode<T>>,
+    }
+    impl<T> Treap<T> {
+        pub fn new(seed: u32) -> Self {
+            Self {
+                rng: XorShift { state: seed },
+                root: None,
+            }
+        }
+        pub fn len(&self) -> usize {
+            size(&self.root)
+        }
+        pub fn nth(&self, k: usize) -> &T {
+            let root = self
+                .root
+                .as_ref()
+                .expect("Cannot fetch the k-th element of an empty set.");
+            root.nth(k)
+        }
+    }
+
+    impl<T: PartialOrd> Treap<T> {
+        pub fn insert(&mut self, value: T) -> bool {
+            let priority = self.rng.next();
+            if let Some(root) = self.root.take() {
+                let (contains, k) = root.find(&value);
+                if !contains {
+                    self.root = Some(insert(Some(root), k, value, priority));
+                    true
+                } else {
+                    self.root = Some(root);
+                    false
+                }
+            } else {
+                self.root = Some(Node::new(value, priority));
+                true
+            }
+        }
+        pub fn contains(&self, value: &T) -> bool {
+            if let Some(root) = self.root.as_ref() {
+                root.find(value).0
+            } else {
+                false
+            }
+        }
+
+        pub fn erase(&mut self, value: &T) -> Option<T> {
+            if let Some(root) = self.root.take() {
+                let (contains, k) = root.find(&value);
+                if !contains {
+                    self.root = Some(root);
+                    None
+                } else {
+                    let (root, removed) = erase(Some(root), k);
+                    self.root = root;
+                    removed.map(|b| b.key)
+                }
+            } else {
+                None
+            }
+        }
+        pub fn binary_search(&self, value: &T) -> Result<usize, usize> {
+            match self.root.as_ref() {
+                Some(root) => {
+                    let (contains, k) = root.find(value);
+                    if contains {
+                        Ok(k)
+                    } else {
+                        Err(k)
+                    }
+                }
+                None => Err(0),
+            }
+        }
+    }
 
     #[derive(Debug)]
     struct Node<T> {
@@ -24,211 +97,107 @@ pub mod treap {
                 count: 1,
             })
         }
-
         fn update_count(&mut self) {
-            self.count = 1 + count(&self.left) + count(&self.right);
+            self.count = size(&self.left) + size(&self.right) + 1;
         }
-    }
-
-    /// `Treap` is a binary search tree data structure that maintain a dynamic set
-    /// of ordered keys and allow binary searches among the keys,
-    /// [https://en.wikipedia.org/wiki/Treap](https://en.wikipedia.org/wiki/Treap)
-    #[derive(Debug)]
-    pub struct Treap<T> {
-        random_state: XorShift,
-        root: Option<BNode<T>>,
-    }
-
-    impl<T> Treap<T> {
-        pub fn new(seed: u32) -> Self {
-            Treap {
-                random_state: XorShift { state: seed },
-                root: None,
-            }
-        }
-        pub fn clear(&mut self) {
-            self.root = None
-        }
-
-        pub fn is_empty(&self) -> bool {
-            self.len() == 0
-        }
-        pub fn len(&self) -> usize {
-            count(&self.root)
-        }
-        pub fn nth(&self, n: usize) -> Option<&T> {
-            if let Some(root) = self.root.as_ref() {
-                rank(root, n).as_ref().map(|node| &node.key)
+        fn nth(&self, k: usize) -> &T {
+            let left_size = size(&self.left);
+            if left_size > k {
+                let left = self.left.as_ref().expect("");
+                left.nth(k)
+            } else if left_size == k {
+                &self.key
             } else {
-                None
+                let right = self.right.as_ref().expect("");
+                right.nth(k - left_size - 1)
             }
         }
     }
 
-    impl<T: PartialOrd> Treap<T> {
-        pub fn insert(&mut self, key: T) -> bool {
-            let (root, inserted) = insert(self.root.take(), key, &mut self.random_state);
-            self.root = Some(root);
-            inserted
-        }
-        pub fn contains(&self, key: &T) -> bool {
-            self.index_of(key).is_some()
-        }
-        pub fn index_of(&self, key: &T) -> Option<usize> {
-            find(&self.root, key)
-        }
-    }
-    impl<T: PartialOrd> Treap<T> {
-        pub fn erase(&mut self, key: &T) -> Option<T> {
-            if let Some(root) = self.root.take() {
-                let (root, removed) = erase(root, EraseOp::ByKey(key));
-                self.root = root;
-                removed
+    impl<T: PartialOrd> Node<T> {
+        fn find(&self, value: &T) -> (bool, usize) {
+            let left_size = size(&self.left);
+            if &self.key == value {
+                (true, left_size)
+            } else if &self.key > value {
+                if let Some(left) = self.left.as_ref() {
+                    left.find(value)
+                } else {
+                    (false, 0)
+                }
             } else {
-                None
+                if let Some(right) = self.right.as_ref() {
+                    let (contained, size) = right.find(value);
+                    (contained, size + left_size + 1)
+                } else {
+                    (false, left_size + 1)
+                }
             }
         }
     }
 
-    fn find<T: PartialOrd>(node: &Option<BNode<T>>, key: &T) -> Option<usize> {
-        match node {
-            None => None,
-            Some(node) => match cmp(&node.key, key) {
-                Equal => Some(count(&node.left)),
-                Greater => find(&node.left, key),
-                Less => match find(&node.right, key) {
-                    None => None,
-                    Some(pos) => Some(count(&node.left) + 1 + pos),
-                },
-            },
+    fn insert<T>(t: Option<BNode<T>>, k: usize, value: T, priority: u32) -> BNode<T> {
+        let (first, second) = split(t, k);
+        let node = merge(first, Some(Node::new(value, priority)));
+        let mut node =
+            merge(node, second).expect("It shouldn't be a none, since one node is added at least.");
+        node.update_count();
+        node
+    }
+    fn erase<T>(node: Option<BNode<T>>, k: usize) -> (Option<BNode<T>>, Option<BNode<T>>) {
+        let (first, second) = split(node, k + 1);
+        let (first, removed) = split(first, k);
+        match merge(first, second) {
+            Some(mut node) => {
+                node.update_count();
+                (Some(node), removed)
+            }
+            None => (None, removed),
         }
     }
 
-    fn count<T>(node: &Option<BNode<T>>) -> usize {
-        node.as_ref().map(|node| node.count).unwrap_or(0)
+    fn merge<T>(s: Option<BNode<T>>, t: Option<BNode<T>>) -> Option<BNode<T>> {
+        match (s, t) {
+            (Some(mut s), Some(mut t)) => {
+                if s.priority > t.priority {
+                    s.right = merge(s.right, Some(t));
+                    s.update_count();
+                    Some(s)
+                } else {
+                    t.left = merge(Some(s), t.left);
+                    t.update_count();
+                    Some(t)
+                }
+            }
+            (Some(s), None) => Some(s),
+            (None, Some(t)) => Some(t),
+            (None, None) => None,
+        }
     }
 
-    fn rotate_left<T>(mut node: BNode<T>, mut right: BNode<T>) -> BNode<T> {
-        node.right = right.left.take();
-        node.update_count();
-        right.left = Some(node);
-        right
-    }
-
-    fn rotate_right<T>(mut node: BNode<T>, mut left: BNode<T>) -> BNode<T> {
-        node.left = left.right.take();
-        node.update_count();
-        left.right = Some(node);
-        left
-    }
-
-    fn insert<T: PartialOrd>(
-        node: Option<BNode<T>>,
-        key: T,
-        rand: &mut XorShift,
-    ) -> (BNode<T>, bool) {
+    fn split<T>(node: Option<BNode<T>>, k: usize) -> (Option<BNode<T>>, Option<BNode<T>>) {
         if let Some(mut node) = node {
-            match cmp(&node.key, &key) {
-                Less => {
-                    let (right, inserted) = insert(node.right.take(), key, rand);
-                    if right.priority < node.priority {
-                        node = rotate_left(node, right);
-                    } else {
-                        node.right = Some(right);
-                    }
-                    node.update_count();
-                    (node, inserted)
-                }
-                Greater => {
-                    let (left, inserted) = insert(node.left.take(), key, rand);
-                    if left.priority < node.priority {
-                        node = rotate_right(node, left);
-                    } else {
-                        node.left = Some(left);
-                    }
-                    node.update_count();
-                    (node, inserted)
-                }
-                Equal => (node, false),
+            let left_size = size(&node.left);
+            if k <= left_size {
+                let left = node.left.take();
+                let (first, second) = split(left, k);
+                node.left = second;
+                node.update_count();
+                (first, Some(node))
+            } else {
+                let right = node.right.take();
+                let (first, second) = split(right, k - left_size - 1);
+                node.right = first;
+                node.update_count();
+                (Some(node), second)
             }
         } else {
-            (Node::new(key, rand.next()), true)
+            (None, None)
         }
     }
 
-    enum EraseOp<'a, T> {
-        ByKey(&'a T),
-        Minimum,
-    }
-
-    fn erase<T: PartialOrd>(mut node: BNode<T>, op: EraseOp<T>) -> (Option<BNode<T>>, Option<T>) {
-        let cmp = match op {
-            EraseOp::ByKey(key) => cmp(&node.key, key),
-            EraseOp::Minimum => Less,
-        };
-        match cmp {
-            Less => {
-                if let Some(right) = node.right.take() {
-                    let (right, removed) = erase(right, op);
-                    node.right = right;
-                    node.update_count();
-                    (Some(node), removed)
-                } else {
-                    (Some(node), None)
-                }
-            }
-            Greater => {
-                if let Some(left) = node.left.take() {
-                    let (left, removed) = erase(left, op);
-                    node.left = left;
-                    node.update_count();
-                    (Some(node), removed)
-                } else {
-                    (Some(node), None)
-                }
-            }
-            Equal => match (node.left.take(), node.right.take()) {
-                (Some(left), Some(right)) => {
-                    let (right, removed) = erase(right, EraseOp::Minimum);
-                    let mut right_min_key = removed.expect("");
-                    swap(&mut right_min_key, &mut node.key);
-                    let removed_value = right_min_key;
-                    node.right = right;
-                    node.left = Some(left);
-                    node.update_count();
-                    (Some(node), Some(removed_value))
-                }
-                (None, Some(right)) => (Some(right), Some(node.key)),
-                (Some(left), None) => (Some(left), Some(node.key)),
-                _ => (None, Some(node.key)),
-            },
-        }
-    }
-
-    fn rank<T>(node: &BNode<T>, nth: usize) -> Option<&BNode<T>> {
-        let left_count = node.left.as_ref().map(|left| left.count).unwrap_or(0);
-        match left_count.cmp(&nth) {
-            Equal => Some(node),
-            Less => {
-                if let Some(right) = node.right.as_ref() {
-                    rank(right, nth - left_count - 1)
-                } else {
-                    None
-                }
-            }
-            Greater => {
-                if let Some(left) = node.left.as_ref() {
-                    rank(left, nth)
-                } else {
-                    unreachable!()
-                }
-            }
-        }
-    }
-
-    fn cmp<T: PartialOrd>(key1: &T, key2: &T) -> Ordering {
-        key1.partial_cmp(key2)
-            .expect("unsortable data is not supported")
+    fn size<T>(node: &Option<BNode<T>>) -> usize {
+        node.as_ref().map(|node| node.count).unwrap_or(0)
     }
 
     #[derive(Debug)]
@@ -256,22 +225,27 @@ pub mod treap {
 mod test {
     use super::treap::*;
     use rand::distributions::Uniform;
+    use rand::prelude::*;
     use rand::{thread_rng, Rng};
     use std::collections::BTreeSet;
 
     #[test]
     fn test_treap_insert_erase() {
         let mut treap = Treap::new(71);
+        let mut rng = StdRng::seed_from_u64(141);
         let max = 1000000;
 
-        for i in 0..max {
+        let mut v = (0..max).collect::<Vec<_>>();
+        v.shuffle(&mut rng);
+        for &i in v.iter() {
             assert!(!treap.contains(&i));
             assert!(treap.insert(i));
             assert!(!treap.insert(i));
             assert!(treap.contains(&i));
         }
 
-        for i in 0..max {
+        v.shuffle(&mut rng);
+        for &i in v.iter() {
             assert!(treap.contains(&i));
             assert_eq!(treap.erase(&i), Some(i));
             assert_eq!(treap.erase(&i), None);
@@ -290,7 +264,7 @@ mod test {
         }
 
         for i in 0..max {
-            assert_eq!(treap.nth(i), Some(&(i * 2)));
+            assert_eq!(treap.nth(i), &(i * 2));
         }
     }
 
@@ -328,8 +302,8 @@ mod test {
 
             assert_eq!(treap.len(), set.len());
             for (i, x) in set.iter().enumerate() {
-                assert_eq!(treap.nth(i), Some(x));
-                assert_eq!(treap.index_of(x), Some(i));
+                assert_eq!(treap.nth(i), x);
+                assert_eq!(treap.binary_search(x), Ok(i));
             }
         }
     }
